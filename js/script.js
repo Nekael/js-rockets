bfb0f294b8f81e27346969191a83767d197865b5
Nekael <48929852+Nekael@users.noreply.github.com>
2020-04-03 18:28:36 +0100

    Made my rocket!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+//this function does this.
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+	//changing class name
+	//document.getElementById('cool').className = 'cool red';
+}
 
 var sayMyName = function (name) {
 	alert('My name is: '+name);
